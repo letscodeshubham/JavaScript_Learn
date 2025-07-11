1dae36c8ee3349c93020dcde3c21369ecd72c3e8
letscodeshubham <iamrocksoo7@gmail.com>
2025-07-11 02:48:45 +0000

    heap & stack

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -2,35 +2,64 @@
 
 //  7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
 
-const score = 100
-const scoreValue = 100.3
+// const score = 100
+// const scoreValue = 100.3
 
-const isLoggedIn = false
-const outsideTemp = null
-let userEmail;
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
 
-// console.log(id === anotherId);
+// // console.log(id === anotherId);
 
-const bigNumber = 2843684362546412345234523n
+// const bigNumber = 2843684362546412345234523n
 
 // Reference (Non primitive)
 
 // Array, Objects, Functions
 
-const heros = ["shaktiman", "naagraj", "doga"];
+// const heros = ["shaktiman", "naagraj", "doga"];
 
-let myObj = {
-    name: "Shubham",
-    age: 22,
-}
+// let myObj = {
+//     name: "Shubham",
+//     age: 22,
+// }
 
 
-const myFunction = function(){
-    console.log("Hello World");
+// const myFunction = function(){
+//     console.log("Hello World");
     
+// }
+
+// console.log(typeof heros);
+
+
+
+
+
+// **********************************************************************
+
+//Memory
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myPetName = "Balvinder"
+
+anotherName = "Sukhvinder"
+
+console.log(anotherName);
+console.log(myPetName);
+
+let user1 = {
+    email: "user@gmail.com",
+    upi: "user@ybl"
 }
 
-console.log(typeof heros);
+let user2 = user1
+
+user2.email = "shubham@gmail.com"
+
+console.log(user1.email);
+console.log(user2.email);
