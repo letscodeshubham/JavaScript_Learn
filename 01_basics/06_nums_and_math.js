1c07cc80392cb57f13c0e7fc0f44692597a15821
letscodeshubham <iamrocksoo7@gmail.com>
2025-07-22 03:17:15 +0000

    math & num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,39 @@
+const score = 400
+
+const balance = new Number(110.05325)
+
+// console.log(balance)
+// console.log(score)
+
+// console.log(balance.toString());
+// console.log(typeof balance.toString());
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.85342
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ********************* Maths *********************
+
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.55));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.7));
+// console.log(Math.min(4, 6, 8, 7, 1));
+// console.log(Math.max(4, 6, 8, 7, 1));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
