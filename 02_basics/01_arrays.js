92531e93f75b2625c08e1fe3227261cff0cac6a3
letscodeshubham <iamrocksoo7@gmail.com>
2025-07-24 03:09:32 +0000

    Array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,50 @@
+// array
+
+const myArray = [1, 2, 3, 4, 5]
+
+
+// const shallowCopy = [...myArray]
+
+// shallowCopy[0] = 88;
+
+// console.log(shallowCopy)
+// console.log(myArray);
+
+const myHeros = ["IronMan", "SuperMan", "Captain"]
+
+const myArr2 = new Array(1, 2, 3, 4, 5)
+
+// Array Methods
+
+// myArr2.push(8)
+// myArr2.push(9)
+// myArr2.pop()
+
+// myArr2.unshift(1)
+// myArr2.shift()
+
+// console.log(myArr2.includes(5));
+// console.log(myArr2.indexOf(5));
+
+// const newArr = myArr2.join()
+
+// console.log(myArr2);
+
+// console.log(typeof newArr);
+
+// //*****slice, splice*********
+
+// console.log("A ", myArr2);
+// const myn1 = myArr2.slice(1, 3) => slice used to shallow copy the array from mentioned initial index include (1) to exclude (3) = [2, 3]
+// console.log(myn1);
+
+// console.log("B ", myArr2);
+// const myn2 = myArr2.splice(1, 3) => splice used to manupulate the array and return the array from mentioned initial index include (1) remove 3 item = [2, 3, 4]
+
+// console.log("C ", myArr2); => modifiied array will be = [1, 5]
+// console.log(myn2);
+
+
+
+
+
